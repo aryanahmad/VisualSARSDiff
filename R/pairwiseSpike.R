d4c90889646ce37fe76d0405e3d682b23d240513
Aryan Ahmad <aryanahmad@Aryans-MacBook-Pro.local>
2021-12-08 18:50:46 -0500

    Commented out and Explained why this code is now legacy

--- R/pairwiseSpike.R
+++ R/pairwiseSpike.R
@@ -1,30 +1,35 @@
-#' Pairwise compare 2 spike protines
-#'
-#' @param spike1 A spike sequence from a FAFSA file.
-#' @param spike2 A spike sequence from a FAFSA file.
-#' @param scoreCheck Bool
-#' @return the score and comparison of the 2 proteins.
-pairwiseSpike <- function(spike1, spike2, scoreCheck) {
-  if(is.null(spike1) || is.null(spike2) || is.null(scoreCheck)){
-    stop()
-  }
-  if(spike1 == "COVID-19"){
-    spike1 <- covid19spike
-  }
-  if(spike2 == "COVID-19"){
-    spike2 <- covid19spike
-  }
-  if(spike1 == "MERS"){
-    spike1 <- mersspike
-  }
-  if(spike2 == "MERS"){
-    spike2 <- mersspike
-  }
-  spike1Sequence <- spike1[[1]]
-  spike2Sequence <- spike2[[1]]
-  spike1String <- c2s(spike1Sequence)
-  spike2String <- c2s(spike2Sequence)
+#This is a legacy method. Comparing 2 spike proteins by pairwise sequencing
+#is too restrictive, and does not provide any analyzing compared
+#to protein-protein interactions that this package has evolved to.
 
-  pairwiseAlignment(spike1String, spike2String, scoreOnly = scoreCheck)
+
+#' #' Pairwise compare 2 spike protines
+#' #'
+#' #' @param spike1 A spike sequence from a FAFSA file.
+#' #' @param spike2 A spike sequence from a FAFSA file.
+#' #' @param scoreCheck Bool
+#' #' @return the score and comparison of the 2 proteins.
+#' pairwiseSpike <- function(spike1, spike2, scoreCheck) {
+#'   if(is.null(spike1) || is.null(spike2) || is.null(scoreCheck)){
+#'     stop()
+#'   }
+#'   if(spike1 == "COVID-19"){
+#'     spike1 <- covid19spike
+#'   }
+#'   if(spike2 == "COVID-19"){
+#'     spike2 <- covid19spike
+#'   }
+#'   if(spike1 == "MERS"){
+#'     spike1 <- mersspike
+#'   }
+#'   if(spike2 == "MERS"){
+#'     spike2 <- mersspike
+#'   }
+#'   spike1Sequence <- spike1[[1]]
+#'   spike2Sequence <- spike2[[1]]
+#'   spike1String <- c2s(spike1Sequence)
+#'   spike2String <- c2s(spike2Sequence)
+#'
+#'   pairwiseAlignment(spike1String, spike2String, scoreOnly = scoreCheck)
 }
 
--- tests/testthat/testpairwiseSpike.R
+++ tests/testthat/testpairwiseSpike.R
@@ -1,15 +1,20 @@
-library("seqinr")
-test_that("pairwise Spike has a low score", {
-  testCall <- pairwiseSpike("COVID-19", "MERS", TRUE)
-  expect_lt(testCall,-3000)
-})
+#This is a legacy method. Comparing 2 spike proteins by pairwise sequencing
+#is too restrictive, and does not provide any analyzing compared
+#to protein-protein interactions that this package has evolved to.
 
-test_that("pairwise Spike has a high score with the same, COVID", {
-  testCall <- pairwiseSpike("COVID-19", "COVID-19", TRUE)
-  expect_gt(testCall, 4000)
-})
 
-test_that("pairwise Spike has a high score with the same, MERS", {
-  testCall <- pairwiseSpike("MERS", "MERS", TRUE)
-  expect_gt(testCall, 4000)
-})
+# library("seqinr")
+# test_that("pairwise Spike has a low score", {
+#   testCall <- pairwiseSpike("COVID-19", "MERS", TRUE)
+#   expect_lt(testCall,-3000)
+# })
+#
+# test_that("pairwise Spike has a high score with the same, COVID", {
+#   testCall <- pairwiseSpike("COVID-19", "COVID-19", TRUE)
+#   expect_gt(testCall, 4000)
+# })
+#
+# test_that("pairwise Spike has a high score with the same, MERS", {
+#   testCall <- pairwiseSpike("MERS", "MERS", TRUE)
+#   expect_gt(testCall, 4000)
+# })
